664f51c121d6e259543377d6726be4df88ce214f
Joao Pedro Oliveira Barbosa <95698131+Iohanan-Cephas@users.noreply.github.com>
2026-09-10 09:48:24 -0300

    create data contract

--- src/test/java/example/cashcard/CashCardJsonTest.java
+++ src/test/java/example/cashcard/CashCardJsonTest.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
 
+import example.cashcard.models.CashCard;
+
 @JsonTest 
 public class CashCardJsonTest {
 
@@ -16,7 +18,7 @@ public class CashCardJsonTest {
     private JacksonTester<CashCard> json;
 
     @Test 
-    void cashCardSerializationTest() throws IOExceptionException {
+    void cashCardSerializationTest() throws IOException {
 
         CashCard cashCard = new CashCard(99L, 123.45);
         assertThat(json.write(cashCard)).isStrictlyEqualToJson("expected.json");
@@ -24,7 +26,6 @@ public class CashCardJsonTest {
         assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.id").isEqualTo(99);
         assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.amount");
         assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.amount").isEqualTo(123.45);
-
 
     }
 
